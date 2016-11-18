b3d85eceec91f8efcada2423080d9bce385a980b
Vishnu N <vishnunarang2000@gmail.com>
2016-11-17 21:11:20 -0700

    Creating basic route for index view for network editor.

---
+++ network_editor/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+
+from . import views
+
+app_name = 'network_editor'  # used in template as 'polls:detail' for eg.
+urlpatterns = [
+    # ex: /network_editor/
+    url(r'^$', views.index, name='index'),
+]
