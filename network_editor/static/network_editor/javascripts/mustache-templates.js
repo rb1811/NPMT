f74b59ed9b9104b1308f5a8d2e2fdf81788481a6
Vishnu N <vishnunarang2000@gmail.com>
2016-11-23 00:50:29 -0700

    Designing sidebar for fault analyzer page

--- network_editor/static/network_editor/javascripts/mustache-templates.js
+++ network_editor/static/network_editor/javascripts/mustache-templates.js
@@ -1,8 +1,8 @@
 window.network_editor = window.network_editor ? window.network_editor : {};
 window.network_editor.templates = {
     node: '<li data-lat="{{lat}}" data-lng="{{lng}}" class="list-group-item node-list-item clearfix">' +
-    '<p>Lat: <span class="node-lat truncate">{{lat}}</span></p>' +
-    '<p>Long: <span class="node-lng truncate">{{lng}}</span></p>' +
+    '<p>Lat: <span class="node-lat text-overflow">{{lat}}</span></p>' +
+    '<p>Long: <span class="node-lng text-overflow">{{lng}}</span></p>' +
     '<input type="button" value="Delete" class="btn btn-xs btn-danger delete-node pull-right"/> ' +
     "<button type='button' class='edit-node btn btn-xs btn-default'  data-trigger='focus' data-container='body' " +
     "data-toggle='popover'" +
@@ -22,8 +22,8 @@ window.network_editor.templates = {
     edge: '<li data-start-lat="{{startLat}}" data-start-lng="{{startLng}}" ' +
     'data-end-lat="{{endLat}}" data-end-lng="{{endLng}}"' +
     'class="list-group-item edge-list-item clearfix">' +
-    '<p class="edge-start">Start: (<span class="truncate">{{startLat}}</span>,<span class="truncate">{{startLng}}</span>)</p>' +
-    '<p class="edge-end">End: (<span class="truncate">{{endLat}}</span>, <span class="truncate">{{endLng}}</span>)</p>' +
+    '<p class="edge-start">Start: (<span class="text-overflow">{{startLat}}</span>,<span class="text-overflow">{{startLng}}</span>)</p>' +
+    '<p class="edge-end">End: (<span class="text-overflow">{{endLat}}</span>, <span class="text-overflow">{{endLng}}</span>)</p>' +
     '<input type="button" value="Delete" class="btn btn-xs btn-danger delete-edge pull-right"> ' +
     '</li>',
 
