46acd1e50df0d5bd4be94b72639a7d00708d56dc
Vishnu N <vishnunarang2000@gmail.com>
2016-11-22 19:00:15 -0700

    Setting home url to default to network_editor app

--- npmtapp/urls.py
+++ npmtapp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', lambda r: HttpResponseRedirect('network_editor/')),
     url(r'^network_editor/', include('network_editor.urls', namespace="network_editor")),
 ]
